07aca72959b99824fd21ea63778066b1b22f9b4c
David Eadie <david.eadie@red-gate.com>
2016-04-15 16:58:53 +0100

    Make exe work using args

--- WormsSchemeConverter/WormsScheme/Program.cs
+++ WormsSchemeConverter/WormsScheme/Program.cs
@@ -6,37 +6,31 @@ namespace WormsScheme
     {
         static void Main(string[] args)
         {
-            var deserialiser = new WscReader(@"F:\Steam\steamapps\common\Worms Armageddon\User\Schemes\{{01}} Beginner.wsc");
-            var model = deserialiser.GetModel();
-
-            var wscCreator = new WscWriter(@"F:\Steam\steamapps\common\Worms Armageddon\User\Schemes\testcreation.wsc");
-            wscCreator.WriteModel(model);
-
-            var textFileCreator = new TextFileWriter(@"F:\Steam\steamapps\common\Worms Armageddon\User\Schemes\01_-_uber_coolest_options.txt");
-            textFileCreator.WriteModel(model);
-
-            var textFileReader = new TextFileReader(@"F:\Steam\steamapps\common\Worms Armageddon\User\Schemes\01_-_uber_coolest_options.txt");
-            var finalModel = textFileReader.GetModel();
-
-
-            Console.WriteLine(model.Signature);
-            Console.WriteLine(model.Version);
-            Console.WriteLine(model.HotSeatDelay);
-            Console.WriteLine(model.RetreatTime);
-            Console.WriteLine(model.RopeRetreatTime);
-            Console.WriteLine(model.HealthCrateProbability);
-            Console.WriteLine(model.SheepHeaven);
-
-            foreach (var weapon in model.Weapons)
-            {
-                Console.WriteLine(weapon.Name);
-                Console.WriteLine(weapon.Ammo);
-                Console.WriteLine(weapon.Power);
-                Console.WriteLine(weapon.Delay);
-                Console.WriteLine(weapon.CrateProbability);
-            }
+            var fileOne = args[0];
+            var fileTwo = args[1];
+            Scheme model;
             
-            Console.ReadKey();
+            if (fileOne.EndsWith(".wsc"))
+            {
+                var wscReader = new WscReader(fileOne);
+                model = wscReader.GetModel();
+            }
+            else
+            {
+                var textFileReader = new TextFileReader(fileOne);
+                model = textFileReader.GetModel();
+            }
+
+            if (fileTwo.EndsWith(".wsc"))
+            {
+                var wscCreator = new WscWriter(fileTwo);
+                wscCreator.WriteModel(model);
+            }
+            else
+            {
+                var textFileCreator = new TextFileWriter(fileTwo);
+                textFileCreator.WriteModel(model);
+            }
         }
     }
 }
