2f49dfb47f256e89db19b7f8d7188f54c12a9492
Michael-Upton <Michael.Upton@red-gate.com>
2016-06-06 15:44:34 +0100

    Tabs to spaces

--- HostWormsGame.ps1
+++ HostWormsGame.ps1
@@ -8,27 +8,27 @@ $wa = Join-Path $installDirPath 'WA.exe'
 $schemesDirPath = Join-Path $installDirPath '\User\Schemes\'
 
 function Get-Ip() {
-	return Get-NetAdapter -Physical | 
+    return Get-NetAdapter -Physical | 
         where {$_.Name -notlike '*VMWare*'} | 
         Get-NetIPAddress -AddressFamily IPv4 |
         select -ExpandProperty IPAddress
 }
 
 function Send-Slack() {
-	$ip = Get-Ip
-	$messageText = "<!here> Hosting at: $ip"
-	$message = @{token=$yourSecretSlackToken; channel=$channel; text=$messageText; as_user=$true}
+    $ip = Get-Ip
+    $messageText = "<!here> Hosting at: $ip"
+    $message = @{token=$yourSecretSlackToken; channel=$channel; text=$messageText; as_user=$true}
     Invoke-RestMethod -Uri https://slack.com/api/chat.postMessage -Body $message
 }
 
 function Update-Options() {
-	$latestrelease = Invoke-RestMethod $optionsRepoUrl
-	$latestrelease.assets | 
+    $latestrelease = Invoke-RestMethod $optionsRepoUrl
+    $latestrelease.assets | 
         where {$_.name -like "*.wsc"} |
-	    foreach {
-		    Write-Host $_.name
-		    Invoke-WebRequest $_.browser_download_url -OutFile (Join-Path $schemesDirPath $_.name)
-	    }
+        foreach {
+            Write-Host $_.name
+            Invoke-WebRequest $_.browser_download_url -OutFile (Join-Path $schemesDirPath $_.name)
+        }
 }
 
 function Start-Worms() {
@@ -38,7 +38,7 @@ function Start-Worms() {
     # http://worms2d.info/WormNET_(Worms_Armageddon)#Channels
     #
     # 'We' means 4 worms per team (don't ask)
-	& $wa wa:host?scheme=We
+    & $wa wa:host?scheme=We
 }
 
 Send-Slack
