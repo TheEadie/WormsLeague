50b8bd0942e704491e52657dd116af517e5dd8fc
David Eadie <david.eadie@red-gate.com>
2017-02-09 16:58:36 +0000

    Add random generator with uber coolest options
    as the default options for now

---
+++ WormsSchemeConverter/WormsScheme/RandomSchemeGenerator.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+
+namespace WormsScheme
+{
+    public class RandomSchemeGenerator
+    {
+        public Scheme GetModel()
+        {
+            const string signature = "SCHM";
+            const int version = 2;
+
+            var hotSeatDelay = 5;
+            var retreatTime = 3;
+            var ropeRetreatTime = 5;
+            var showRoundTime = true;
+            var automaticReplays = false;
+            var fallDamage = 1;
+            var artilleryMode = false;
+            byte stockpilingMode = 0;
+            byte wormSelect = 0;
+            byte suddenDeathEvent = 3;
+            var waterRiseRate = 2;
+            var weaponCrateProbability = 80;
+            var donorCards = true;
+            var healthCrateProbability = 20;
+            var healthCrateEnergy = 50;
+            var utilityCrateProbability = 20;
+            var hazardObjectTypes = 43;
+            var mineDelay = 0;
+            var dudMines = false;
+            var wormPlacement = false;
+            var initialWormEnergy = 100;
+            var turnTime = 45;
+            var roundTime = 10;
+            var numberOfRounds = 1;
+            var blood = false;
+            var aquaSheep = true;
+            var sheepHeaven = false;
+            var godWorms = false;
+            var indestructibleLand = false;
+            var upgradedGrenade = false;
+            var upgradedShotgun = false;
+            var upgradedClusterBombs = false;
+            var upgradedLongbow = false;
+            var teamWeapons = false;
+            var superWeapons = false;
+
+            var weapons = new List<Weapon>();
+
+            foreach (var weaponName in Weapons.AllWeapons)
+            {
+                var ammo = 0;
+                var power = 0;
+                var delay = 0;
+                var crateProb = 0;
+
+                weapons.Add(new Weapon(weaponName, ammo, power, delay, crateProb));
+            }
+
+            return new Scheme(
+                signature,
+                version,
+                hotSeatDelay,
+                retreatTime,
+                ropeRetreatTime,
+                showRoundTime,
+                automaticReplays,
+                fallDamage,
+                artilleryMode,
+                stockpilingMode,
+                wormSelect,
+                suddenDeathEvent,
+                waterRiseRate,
+                weaponCrateProbability,
+                donorCards,
+                healthCrateProbability,
+                healthCrateEnergy,
+                utilityCrateProbability,
+                hazardObjectTypes,
+                mineDelay,
+                dudMines,
+                wormPlacement,
+                initialWormEnergy,
+                turnTime,
+                roundTime,
+                numberOfRounds,
+                blood,
+                aquaSheep,
+                sheepHeaven,
+                godWorms,
+                indestructibleLand,
+                upgradedGrenade,
+                upgradedShotgun,
+                upgradedClusterBombs,
+                upgradedLongbow,
+                teamWeapons,
+                superWeapons,
+                weapons);
+        }
+    }
+}
