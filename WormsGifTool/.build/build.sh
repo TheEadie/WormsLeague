92563f3b126ce9af3779f3614662568a23d69e3c
David Eadie <david.eadie@red-gate.com>
2019-11-15 13:45:01 +0000

    Add linux build script for gif tool

---
+++ WormsGifTool/.build/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+dotnet test ./GifTool.Tests/GifTool.Tests.csproj
+dotnet publish ./GifTool/GifTool.csproj -r win10-x64 --self-contained -c release -o ./release /p:PublishSingleFile=true
