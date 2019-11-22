dc345d19d48535e7840fe570026928f40ab8bca3
David Eadie <david.eadie@red-gate.com>
2019-11-22 16:04:20 +0000

    Update the .wsc files in the new location

--- Schemes/.build/build.ps1
+++ Schemes/.build/build.ps1
@@ -3,5 +3,5 @@ $versionInfo | Out-File "release/version.json"
 
 $version = $versionInfo.MajorMinorPatch
 
-dotnet SchemeConverter/WormsSchemeConverter.dll "Schemes/Uber Coolest Options.txt" "release/Uber.Coolest.Options.$version.wsc"
-dotnet SchemeConverter/WormsSchemeConverter.dll "Schemes/Speed Worms.txt" "release/Speed.Worms.$version.wsc"
+dotnet ../SchemeConverter/WormsSchemeConverter.dll "Uber Coolest Options.txt" "release/Uber.Coolest.Options.$version.wsc"
+dotnet ../SchemeConverter/WormsSchemeConverter.dll "Speed Worms.txt" "release/Speed.Worms.$version.wsc"
