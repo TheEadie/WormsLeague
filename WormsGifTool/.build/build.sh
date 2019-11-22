fad832e02bc874baaef846f21fa2997d7f724e5b
David Eadie <david.eadie@red-gate.com>
2019-11-22 13:25:50 +0000

    Fix release script for schemes

--- WormsGifTool/.build/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dotnet test ./GifTool.Tests/GifTool.Tests.csproj
-dotnet publish ./GifTool/GifTool.csproj -r win10-x64 --self-contained -c release -o ./release /p:PublishSingleFile=true
