732bf8e68034e5b2099ffad029c87ab6e3233b0b
David Eadie <david.a.eadie@gmail.com>
2019-11-22 16:32:00 +0000

    Move schemes to a build script

---
+++ Schemes/.build/build.ps1
@@ -0,0 +1,8 @@
+$versionInfo = gitversion | Out-String
+$versionInfo | Out-File "release/version.json"
+
+$json = $versionInfo | ConvertFrom-Json
+$version = $json.MajorMinorPatch
+
+dotnet ../SchemeConverter/WormsSchemeConverter.dll "Uber Coolest Options.txt" "release/Uber.Coolest.Options.$version.wsc"
+dotnet ../SchemeConverter/WormsSchemeConverter.dll "Speed Worms.txt" "release/Speed.Worms.$version.wsc"
