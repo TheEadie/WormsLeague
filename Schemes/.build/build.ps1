65656880fe9b25be68d4adbbb1f83e3dac58e9cf
David Eadie <david.a.eadie@gmail.com>
2019-11-22 16:49:19 +0000

    Add new scheme to build

--- Schemes/.build/build.ps1
+++ Schemes/.build/build.ps1
@@ -6,3 +6,4 @@ $version = $json.MajorMinorPatch
 
 dotnet ../SchemeConverter/WormsSchemeConverter.dll "Uber Coolest Options.txt" "release/Uber.Coolest.Options.$version.wsc"
 dotnet ../SchemeConverter/WormsSchemeConverter.dll "Speed Worms.txt" "release/Speed.Worms.$version.wsc"
+dotnet ../SchemeConverter/WormsSchemeConverter.dll "Christmas 2019.txt" "release/Christmas.$version.wsc"
