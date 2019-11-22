1d6dfbc049ff2e72cf53b5c20b18f882fe49f51a
David Eadie <david.eadie@red-gate.com>
2019-11-22 15:55:37 +0000

    Generate version number for release

--- Schemes/.build/build.ps1
+++ Schemes/.build/build.ps1
@@ -1,4 +1,7 @@
-dotnet SchemeConverter/WormsSchemeConverter.dll "Schemes/Uber Coolest Options.txt" "release/Uber.Coolest.Options.$(date +'%Y%m%d').wsc"
-dotnet SchemeConverter/WormsSchemeConverter.dll "Schemes/Speed Worms.txt" "release/Speed.Worms.$(date +'%Y%m%d').wsc"
+$versionInfo = gitversion
+$versionInfo | Out-File "release/version.json"
 
-gitversion | Out-File "release/version.json"
+$version = $versionInfo.MajorMinorPatch
+
+dotnet SchemeConverter/WormsSchemeConverter.dll "Schemes/Uber Coolest Options.txt" "release/Uber.Coolest.Options.$version.wsc"
+dotnet SchemeConverter/WormsSchemeConverter.dll "Schemes/Speed Worms.txt" "release/Speed.Worms.$version.wsc"
