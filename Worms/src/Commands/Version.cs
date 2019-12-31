a53f225029c56bdf76452a888b810624581da36c
David Eadie <david.eadie@red-gate.com>
2019-12-31 11:53:42 +0000

    Add game version to Version command output

--- Worms/src/Commands/Version.cs
+++ Worms/src/Commands/Version.cs
@@ -1,16 +1,28 @@
+using System.Diagnostics;
 using System.Reflection;
 using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
+using Worms.GameRunner;
 
 namespace Worms.Commands
 {
     [Command("version", Description = "Get the current version of the worms CLI")]
     public class Version
     {
+        private readonly IWormsLocator _wormsLocator;
+
+        public Version(IWormsLocator wormsLocator)
+        {
+            _wormsLocator = wormsLocator;
+        }
+
         public Task<int> OnExecuteAsync(IConsole console)
         {
-            var versionString = Assembly.GetEntryAssembly().GetName().Version.ToString(3);
-            console.WriteLine($"Worms {versionString}");
+            var cliVersion = Assembly.GetEntryAssembly().GetName().Version.ToString(3);
+            console.WriteLine($"Worms CLI: {cliVersion}");
+
+            var gameVersion = FileVersionInfo.GetVersionInfo(_wormsLocator.ExeLocation).ProductVersion;
+            console.WriteLine($"Worms Armageddon: {gameVersion}");
             return Task.FromResult(0);
         }
     }
