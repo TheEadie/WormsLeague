91c96d6810b96b1747d91996d34d200f8a3c57c9
David Eadie <david.eadie@red-gate.com>
2020-01-17 14:28:53 +0000

    Add new project for update command line

---
+++ CommandLine/src/Update.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Update.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
