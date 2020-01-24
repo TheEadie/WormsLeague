6b6cc9fe013c0ada614dd4de8c98301d100f4469
David Eadie <david.eadie@red-gate.com>
2020-01-24 14:50:14 +0000

    Add configure await as Rosylnator is unhappy without it

--- CommandLine/src/Cli/PackageManagers/GitHubReleasesPackageManager.cs
+++ CommandLine/src/Cli/PackageManagers/GitHubReleasesPackageManager.cs
@@ -36,7 +36,7 @@ namespace Worms.Updates.PackageManagers
 
         public async Task<IEnumerable<Version>> GetAvailableVersions()
         {
-            var releases = await _gitHubClient.Repository.Release.GetAll(_repoOwner, _repoName);
+            var releases = await _gitHubClient.Repository.Release.GetAll(_repoOwner, _repoName).ConfigureAwait(false);
             var matching = releases.Where(x => x.TagName.StartsWith(_tagPrefix));
             var tagVersions = matching.Select(x => x.TagName.Replace(_tagPrefix, string.Empty));
 
@@ -53,14 +53,14 @@ namespace Worms.Updates.PackageManagers
 
         public async Task DownloadVersion(Version version, string downloadToFolderPath)
         {
-            var releases = await _gitHubClient.Repository.Release.GetAll(_repoOwner, _repoName);
+            var releases = await _gitHubClient.Repository.Release.GetAll(_repoOwner, _repoName).ConfigureAwait(false);
             var matching = releases.Single(x => x.TagName == _tagPrefix + version.ToString(3));
-            var files = await _gitHubClient.Repository.Release.GetAllAssets(_repoOwner, _repoName, matching.Id);
+            var files = await _gitHubClient.Repository.Release.GetAllAssets(_repoOwner, _repoName, matching.Id).ConfigureAwait(false);
 
             foreach(var file in files)
             {
-                var raw = await _gitHubClient.Connection.Get<byte[]>(new Uri(file.Url), new Dictionary<string, string>(), "application/octet-stream");
-                await File.WriteAllBytesAsync(Path.Combine(downloadToFolderPath, file.Name), raw.Body);
+                var raw = await _gitHubClient.Connection.Get<byte[]>(new Uri(file.Url), new Dictionary<string, string>(), "application/octet-stream").ConfigureAwait(false);
+                await File.WriteAllBytesAsync(Path.Combine(downloadToFolderPath, file.Name), raw.Body).ConfigureAwait(false);
             }
         }
     }
