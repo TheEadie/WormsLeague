8d8769cd55c34d9366e2e4e470ee27b9481ddf0d
David Eadie <david@eadie.net>
2020-04-19 14:35:39 +0100

    Add linux support for my chromebook only
    TODO: use a config setting for the install directory so that anyone can run this

--- CommandLine/src/WormsArmageddon/Linux/WormsLocator.cs
+++ CommandLine/src/WormsArmageddon/Linux/WormsLocator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace Worms.WormsArmageddon.Linux
 {
@@ -6,7 +8,16 @@ namespace Worms.WormsArmageddon.Linux
     {
         public GameInfo Find()
         {
-            return new GameInfo(false, string.Empty, string.Empty, new Version(0, 0, 0, 0), string.Empty);
+            const string processName = "WA";
+            var userHomeDirectory = Environment.GetEnvironmentVariable("HOME");
+            var rootLocation = Path.Combine(userHomeDirectory, "games", "worms");
+            var exeLocation = Path.Combine(rootLocation, processName + ".exe");
+            var schemesFolder = Path.Combine(rootLocation, "User", "Schemes");
+
+            var versionInfo = FileVersionInfo.GetVersionInfo(exeLocation);
+            var version = new Version(versionInfo.ProductMajorPart, versionInfo.ProductMinorPart, versionInfo.ProductBuildPart, versionInfo.ProductPrivatePart);
+
+            return new GameInfo(true, exeLocation, processName, version, schemesFolder);
         }
     }
 }
