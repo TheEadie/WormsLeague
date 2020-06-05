13ab2228e06c8826a065b90914ecc2c52df57dc3
David Eadie <david@eadie.net>
2020-06-05 15:32:33 +0100

    Fix a number of bugs with column widths

--- CommandLine/src/Logging/TableOutput/TableBuilder.cs
+++ CommandLine/src/Logging/TableOutput/TableBuilder.cs
@@ -7,6 +7,7 @@ namespace Worms.Logging.TableOutput
     {
         private readonly int _outputWidth;
         private readonly List<TableColumn> _columns = new List<TableColumn>();
+        private const int _columnPadding = 3;
 
         public TableBuilder(int outputWidth)
         {
@@ -20,22 +21,29 @@ namespace Worms.Logging.TableOutput
 
         public Table Build()
         {
+            // If there's no columns return early
             if (!_columns.Any())
             {
                 return new Table(_columns, 0);
             }
 
+            // Add columns for the amount of screen space
             var adjustedColumns = new List<TableColumn>();
             var currentWidth = 0;
 
-            foreach (var column in _columns.TakeWhile(column => currentWidth <= _outputWidth))
+            foreach (var column in _columns)
             {
+                // Only add another column if the heading can be rendered
+                var headingLength = column.Heading.Length + _columnPadding;
+                if (currentWidth >= _outputWidth - headingLength)
+                    break;
+
                 adjustedColumns.Add(column);
                 currentWidth += column.Width;
             }
 
+            // Adjust the last column so it doesn't wrap the line
             var lastColumn = adjustedColumns.Last();
-
             adjustedColumns.Remove(lastColumn);
             var remainingWidth = _outputWidth - adjustedColumns.Sum(x => x.Width) - 1;
 
@@ -50,14 +58,21 @@ namespace Worms.Logging.TableOutput
 
         private static string TrimText(string input, int maxLength)
         {
-            return input.Length <= maxLength ? input : input.Substring(0, maxLength - 4) + " ...";
+            const string truncate = "..";
+
+            if (maxLength <= truncate.Length)
+            {
+                return input.Length <= maxLength ? input : input.Substring(0, maxLength);
+            }
+
+            return input.Length <= maxLength ? input : input.Substring(0, maxLength - truncate.Length) + truncate;
         }
 
         private static int GetWidth(string header, IReadOnlyCollection<string> values)
         {
             var anyItems = values.Any();
-            var headerLength = header.Length + 3;
-            var longest = anyItems ? values.Max(x => x.Length) + 3 : headerLength;
+            var headerLength = header.Length + _columnPadding;
+            var longest = anyItems ? values.Max(x => x.Length) + _columnPadding : headerLength;
             if (longest < headerLength)
                 longest = headerLength;
             return longest;
