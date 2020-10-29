19d92d361cd4b9c970f76f1aeb2b7d0b317c1d8a
David Eadie <david@eadie.net>
2020-10-29 14:49:16 +0000

    Use docker for builds

--- Server/.build/build.sh
+++ Server/.build/build.sh
@@ -4,26 +4,12 @@ ScriptDir="${BASH_SOURCE%/*}"
 SharedScripts="$ScriptDir/../../.build/shared"
 source "$SharedScripts/logging.sh"
 source "$SharedScripts/calculate-version.sh"
-source "$SharedScripts/artifacts.sh"
-source "$SharedScripts/dotnet.sh"
+source "$SharedScripts/docker.sh"
 
 # Input
 UseDocker=$1
-OutputDir=$2
-Platform=$3
+ImageName=$2
 
 #Build
-PlatformOutputDir="$OutputDir/$Platform"
-
 CalculateVersion "$UseDocker" "Server"
-CleanArtifacts "$PlatformOutputDir"
-
-WriteHeading "Publishing $Platform v$Version_MajorMinorPatch..."
-
-WriteInfo "Building $Platform"
-Dotnet-Publish false "src/Worms.Gateway/Worms.Gateway.csproj" $PlatformOutputDir $Version_MajorMinorPatch $Platform
-
-WriteInfo "Writing version info to $PlatformOutputDir/version.json"
-echo $Version_Json > $PlatformOutputDir/version.json
-
-WriteHighlight "Published to $PlatformOutputDir/"
+BuildDockerImage $ImageName $Version_Major $Version_Minor $Version_Patch
