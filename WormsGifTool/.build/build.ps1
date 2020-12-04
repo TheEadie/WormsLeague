71be53869025d624828f64e5ff96ffeca431035a
David Eadie <david@eadie.net>
2020-12-04 13:52:54 +0000

    Restore single exe step from .NET Core 3

--- WormsGifTool/.build/build.ps1
+++ WormsGifTool/.build/build.ps1
@@ -1,4 +1,4 @@
 dotnet test ./GifTool.Tests/GifTool.Tests.csproj
-dotnet publish ./GifTool/GifTool.csproj -r win10-x64 --self-contained -c release -o ./release /p:PublishSingleFile=true
+dotnet publish ./GifTool/GifTool.csproj -r win10-x64 --self-contained -c release -o ./release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 
 gitversion | Out-File "release/version.json"
