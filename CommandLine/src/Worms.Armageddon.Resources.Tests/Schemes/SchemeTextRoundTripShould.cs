271fc3afd267e16c0cf6e3ebfe096ab9d26946bd
David Eadie <david@eadie.net>
2021-03-19 16:10:52 +0000

    Add round trip tests for schemes to text

---
+++ CommandLine/src/Worms.Armageddon.Resources.Tests/Schemes/SchemeTextRoundTripShould.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using Shouldly;
+using Syroot.Worms.Armageddon;
+using Worms.Armageddon.Resources.Schemes.Text;
+
+namespace Worms.Armageddon.Resources.Tests.Schemes
+{
+    public class SchemeTextRoundTripShould
+    {
+        private SchemeTextWriter _writer;
+        private SchemeTextReader _reader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _writer = new SchemeTextWriter();
+            _reader = new SchemeTextReader();
+        }
+
+        [Test, TestCaseSource(nameof(Schemes))]
+        public void NotLoseInformation(Scheme input)
+        {
+            var result = RoundTrip(input);
+            result.ShouldBe(input);
+        }
+
+        private Scheme RoundTrip(Scheme scheme)
+        {
+            var output = new StringWriter();
+            _writer.Write(scheme, output);
+            var input = output.ToString();
+            var result = _reader.GetModel(input);
+            return result;
+        }
+
+        private static IEnumerable<TestCaseData> Schemes()
+        {
+            foreach (var property in typeof(Scheme).GetProperties())
+            {
+                var scheme = new Scheme {ObjectCount = 10};
+
+                if (property.PropertyType == typeof(bool))
+                {
+                    property.SetValue(scheme, true);
+                }
+                else if (property.PropertyType == typeof(byte))
+                {
+                    property.SetValue(scheme, (byte) 10);
+                }
+                else if (property.PropertyType == typeof(int))
+                {
+                    property.SetValue(scheme, 10);
+                }
+                else if (property.PropertyType == typeof(sbyte))
+                {
+                    property.SetValue(scheme, (sbyte)10);
+                }
+                else if (property.PropertyType == typeof(Stockpiling))
+                {
+                    property.SetValue(scheme, Stockpiling.On);
+                }
+                else if (property.PropertyType == typeof(WormSelect))
+                {
+                    property.SetValue(scheme, WormSelect.Random);
+                }
+                else if (property.PropertyType == typeof(SuddenDeathEvent))
+                {
+                    property.SetValue(scheme, SuddenDeathEvent.NuclearStrike);
+                }
+                else if (property.PropertyType == typeof(MapObjectType))
+                {
+                    property.SetValue(scheme, MapObjectType.OilDrums);
+                }
+                else
+                {
+                    continue;
+                }
+
+                yield return new TestCaseData(scheme).SetName(property.Name);
+            }
+
+            foreach (var property in typeof(Scheme.ExtendedOptions).GetProperties())
+            {
+                var scheme = new Scheme {ObjectCount = 10};
+
+                if (property.PropertyType == typeof(bool))
+                {
+                    property.SetValue(scheme.Extended, true);
+                }
+                else if (property.PropertyType == typeof(byte))
+                {
+                    property.SetValue(scheme.Extended, (byte) 5);
+                }
+                else if (property.PropertyType == typeof(int))
+                {
+                    property.SetValue(scheme.Extended, 10);
+                }
+                else if (property.PropertyType == typeof(sbyte))
+                {
+                    property.SetValue(scheme.Extended, (sbyte)10);
+                }
+                else if (property.PropertyType == typeof(WormPhasing))
+                {
+                    property.SetValue(scheme.Extended, WormPhasing.WormsWeapons);
+                }
+                else if (property.PropertyType == typeof(RopeRollDrops))
+                {
+                    property.SetValue(scheme.Extended, RopeRollDrops.RopeJump);
+                }
+                else if (property.PropertyType == typeof(XImpactControlLoss))
+                {
+                    property.SetValue(scheme.Extended, XImpactControlLoss.Loss);
+                }
+                else if (property.PropertyType == typeof(SkimControlLoss))
+                {
+                    property.SetValue(scheme.Extended, SkimControlLoss.KeptWithRope);
+                }
+                else if (property.PropertyType == typeof(SkipWalk))
+                {
+                    property.SetValue(scheme.Extended, SkipWalk.Facilitated);
+                }
+                else if (property.PropertyType == typeof(Roofing))
+                {
+                    property.SetValue(scheme.Extended, Roofing.Up);
+                }
+                else if (property.PropertyType == typeof(RwGravityType))
+                {
+                    property.SetValue(scheme.Extended, RwGravityType.BlackHoleConstant);
+                }
+                else if (property.PropertyType == typeof(HealthCure))
+                {
+                    property.SetValue(scheme.Extended, HealthCure.Nobody);
+                }
+                else if (property.PropertyType == typeof(SheepHeavenFlags))
+                {
+                    property.SetValue(scheme.Extended, SheepHeavenFlags.Odds);
+                }
+                else
+                {
+                    continue;
+                }
+
+                yield return new TestCaseData(scheme).SetName(property.Name);
+            }
+
+        }
+
+    }
+}
