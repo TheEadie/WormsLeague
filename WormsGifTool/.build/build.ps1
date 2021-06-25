1195cc6951be2d516ca46000d5e0e4cbf958f129
David Eadie <david@eadie.net>
2021-06-25 16:31:29 +0100

    Fix build to use latest gitversion via dotnet global tool

--- WormsGifTool/.build/build.ps1
+++ WormsGifTool/.build/build.ps1
@@ -1,4 +1,8 @@
 dotnet test ./GifTool.Tests/GifTool.Tests.csproj
 dotnet publish ./GifTool/GifTool.csproj -r win10-x64 --self-contained -c release -o ./release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 
-gitversion | Out-File "release/version.json"
+if (-not (dotnet tool list -g | Select-String "gitversion")) {
+    dotnet tool install -g gitversion.tool
+}
+
+dotnet-gitversion | Out-File "release/version.json"
