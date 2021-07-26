8855f557075a738ab25a655f913a505bd471ca09
David Eadie <david@eadie.net>
2021-07-26 15:34:53 +0100

    Use git commands to calculate next version
    GitVersion just keeps breaking and its only a few lines of PoSh anyway

--- WormsGifTool/.build/build.ps1
+++ WormsGifTool/.build/build.ps1
@@ -1,8 +1,17 @@
+. ".build/logging.ps1"
+. ".build/version.ps1"
+
+# Config
+$nextVersion = [System.Version]::Parse("1.1")
+$tag = "giftool/v"
+
+$nextVersion = Get-NextVersion $tag $nextVersion
+
+Write-Heading "Building $nextVersion"
+
 dotnet test ./GifTool.Tests/GifTool.Tests.csproj
-dotnet publish ./GifTool/GifTool.csproj -r win10-x64 --self-contained -c release -o ./release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish ./GifTool/GifTool.csproj -r win10-x64 --self-contained -c release -o ./.artifacts /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:Version=$nextVersion /p:DebugType=none
 
-if (-not (dotnet tool list -g | Select-String "gitversion")) {
-    dotnet tool install -g gitversion.tool
-}
-
-dotnet-gitversion | Out-File "release/version.json"
+$jsonBase = @{}
+$jsonBase.Add("MajorMinorPatch", $nextVersion.ToString())
+$jsonBase | ConvertTo-Json -Depth 10 | Out-File ".artifacts/version.json"
---
+++ WormsGifTool/.build/logging.ps1
@@ -0,0 +1,6 @@
+function Write-Heading ([string] $text)
+{
+    Write-Host "################" -ForegroundColor Blue
+    Write-Host "$text" -ForegroundColor Blue
+    Write-Host "################" -ForegroundColor Blue
+}
