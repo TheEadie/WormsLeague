f8c57f95fcf40a1a1726a9addae7e8e8fb6fe1fd
David Eadie <david@eadie.net>
2022-07-01 15:59:33 +0100

    Fix migration and set up CI

--- database/migrations/V0.1__AddGames.sql
+++ database/migrations/V0.1__AddGames.sql
@@ -1,3 +1,13 @@
+CREATE SEQUENCE IF NOT EXISTS public.games_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+ALTER SEQUENCE public.games_id_seq
+    OWNER TO "spawn_admin_eZiH";
+
 CREATE TABLE IF NOT EXISTS public.games
 (
     id integer NOT NULL DEFAULT nextval('games_id_seq'::regclass),
@@ -12,3 +22,6 @@ TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.games
     OWNER to "spawn_admin_eZiH";
+
+ALTER SEQUENCE public.games_id_seq
+    OWNED BY games.id;
