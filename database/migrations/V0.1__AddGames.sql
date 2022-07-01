baa9c24135cfc62e447bdbbb6e2662f2a380c3bb
David Eadie <david@eadie.net>
2022-07-01 16:32:46 +0100

    Don't use hardcoded users in migration script

--- database/migrations/V0.1__AddGames.sql
+++ database/migrations/V0.1__AddGames.sql
@@ -5,9 +5,6 @@ CREATE SEQUENCE IF NOT EXISTS public.games_id_seq
     MAXVALUE 2147483647
     CACHE 1;
 
-ALTER SEQUENCE public.games_id_seq
-    OWNER TO "spawn_admin_eZiH";
-
 CREATE TABLE IF NOT EXISTS public.games
 (
     id integer NOT NULL DEFAULT nextval('games_id_seq'::regclass),
@@ -19,9 +16,6 @@ WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.games
-    OWNER to "spawn_admin_eZiH";
 
 ALTER SEQUENCE public.games_id_seq
     OWNED BY games.id;
