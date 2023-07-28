89b0dba0eb1ae30489bd6b47939fa5fcadb8742a
David Eadie <david@eadie.net>
2023-07-28 15:29:33 +0100

    Fix report output

--- build/flyway-summary.sh
+++ build/flyway-summary.sh
@@ -9,8 +9,8 @@ function join_by {
   fi
 }
 
-NUMOFADDITIONS=$(jq '.individualResults[] | select(.operation=="changes") | .onlyTarget | length' $PATHTOJSON -r)
-NUMOFDELETIONS=$(jq '.individualResults[] | select(.operation=="changes") | .onlySource | length' $PATHTOJSON -r)
+NUMOFADDITIONS=$(jq '.individualResults[] | select(.operation=="changes") | .onlyInTarget | length' $PATHTOJSON -r)
+NUMOFDELETIONS=$(jq '.individualResults[] | select(.operation=="changes") | .onlyInSource | length' $PATHTOJSON -r)
 NUMOFCHANGES=$(jq '.individualResults[] | select(.operation=="changes") | .differences | length' $PATHTOJSON -r)
 
 
@@ -18,7 +18,7 @@ printf "<h3>Database Changes</h3>"
 printf "✅ Objects to be created: <b>"$NUMOFADDITIONS"</b>"
 
 if [ "$NUMOFADDITIONS" -gt 0 ]; then
-    ADDITIONS=$(jq '.individualResults[] | select(.operation=="changes").onlyTarget[] | (.objectType + "&nbsp;-&nbsp;" + .schema + "." + .name) ' $PATHTOJSON -r)
+    ADDITIONS=$(jq '.individualResults[] | select(.operation=="changes").onlyInTarget[] | (.objectType + "&nbsp;-&nbsp;" + .schema + "." + .name) ' $PATHTOJSON -r)
     join_by "<br/>&nbsp;| ✅ " $ADDITIONS
 fi
 
@@ -30,7 +30,7 @@ fi
 
 printf "<br/>⚠ Objects to be dropped: <b>"$NUMOFDELETIONS"</b>"
 if [ "$NUMOFDELETIONS" -gt 0 ]; then
-    DELETIONS=$(jq '.individualResults[] | select(.operation=="changes").onlySource[] | (.objectType + "&nbsp;-&nbsp;" + .schema + "." + .name) ' $PATHTOJSON -r)
+    DELETIONS=$(jq '.individualResults[] | select(.operation=="changes").onlyInSource[] | (.objectType + "&nbsp;-&nbsp;" + .schema + "." + .name) ' $PATHTOJSON -r)
     join_by "<br/>&nbsp;| ⚠ " $DELETIONS
 fi
 
