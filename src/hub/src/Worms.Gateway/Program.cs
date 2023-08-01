c18d9ecf9d9efe16681755c19b5becf6e01c63ea
David Eadie <david@eadie.net>
2023-08-01 10:04:56 +0100

    Remove need for Startup.cs

--- src/hub/src/Worms.Gateway/Program.cs
+++ src/hub/src/Worms.Gateway/Program.cs
@@ -1,7 +1,17 @@
-using Microsoft.AspNetCore.Hosting;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Worms.Gateway.Announcers;
+using Worms.Gateway.Announcers.Slack;
+using Worms.Gateway.Database;
+using Worms.Gateway.Domain;
+using Worms.Gateway.Dtos;
+using Worms.Gateway.Validators;
 
 namespace Worms.Gateway;
 
@@ -9,12 +19,50 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Logging.AddSimpleConsole(options => { options.SingleLine = true; });
+        builder.Configuration.AddEnvironmentVariables("WORMS_");
+        builder.Services.AddControllers();
+        builder.Services.AddApiVersioning();
+        builder.Services.AddAuthentication(
+                options =>
+                    {
+                        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    })
+            .AddJwtBearer(
+                options =>
+                    {
+                        options.Authority = "https://eadie.eu.auth0.com/";
+                        options.Audience = "worms.davideadie.dev";
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            NameClaimType = ClaimTypes.NameIdentifier,
+                            RoleClaimType = "permissions"
+                        };
+                    });
+        builder.Services.AddAuthorization();
+        RegisterServices(builder);
+
+        var app = builder.Build();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureLogging((_, builder) => builder.AddSimpleConsole(options => { options.SingleLine = true; }))
-            .ConfigureAppConfiguration((_, config) => config.AddEnvironmentVariables("WORMS_"))
-            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+    private static void RegisterServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<IRepository<GameDto>, GamesRepository>();
+        builder.Services.AddSingleton<IRepository<Replay>, ReplaysRepository>();
+        builder.Services.AddSingleton<ISlackAnnouncer, SlackAnnouncer>();
+        builder.Services.AddSingleton<ReplayFileValidator>();
+    }
 }
