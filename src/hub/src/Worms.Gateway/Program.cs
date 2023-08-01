cea675bf85455738fb4aec6d031808e4a3989e08
David Eadie <david@eadie.net>
2023-08-01 10:08:37 +0100

    Remove need for namespace/class/method in Program.cs

--- src/hub/src/Worms.Gateway/Program.cs
+++ src/hub/src/Worms.Gateway/Program.cs
@@ -12,51 +12,38 @@ using Worms.Gateway.Domain;
 using Worms.Gateway.Dtos;
 using Worms.Gateway.Validators;
 
-namespace Worms.Gateway;
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddSimpleConsole(options => { options.SingleLine = true; });
+builder.Configuration.AddEnvironmentVariables("WORMS_");
+builder.Services.AddControllers();
+builder.Services.AddApiVersioning();
+builder.Services.AddAuthentication()
+    .AddJwtBearer(
+        options =>
+            {
+                options.Authority = "https://eadie.eu.auth0.com/";
+                options.Audience = "worms.davideadie.dev";
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = ClaimTypes.NameIdentifier,
+                    RoleClaimType = "permissions"
+                };
+            });
+builder.Services.AddAuthorization();
+builder.Services.AddSingleton<IRepository<GameDto>, GamesRepository>();
+builder.Services.AddSingleton<IRepository<Replay>, ReplaysRepository>();
+builder.Services.AddSingleton<ISlackAnnouncer, SlackAnnouncer>();
+builder.Services.AddSingleton<ReplayFileValidator>();
 
-public static class Program
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Logging.AddSimpleConsole(options => { options.SingleLine = true; });
-        builder.Configuration.AddEnvironmentVariables("WORMS_");
-        builder.Services.AddControllers();
-        builder.Services.AddApiVersioning();
-        builder.Services.AddAuthentication()
-            .AddJwtBearer(
-                options =>
-                    {
-                        options.Authority = "https://eadie.eu.auth0.com/";
-                        options.Audience = "worms.davideadie.dev";
-                        options.TokenValidationParameters = new TokenValidationParameters
-                        {
-                            NameClaimType = ClaimTypes.NameIdentifier,
-                            RoleClaimType = "permissions"
-                        };
-                    });
-        builder.Services.AddAuthorization();
-        RegisterServices(builder);
-
-        var app = builder.Build();
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseRouting();
-        app.UseAuthentication();
-        app.UseAuthorization();
-        app.MapControllers();
-        app.Run();
-    }
-
-    private static void RegisterServices(WebApplicationBuilder builder)
-    {
-        builder.Services.AddSingleton<IRepository<GameDto>, GamesRepository>();
-        builder.Services.AddSingleton<IRepository<Replay>, ReplaysRepository>();
-        builder.Services.AddSingleton<ISlackAnnouncer, SlackAnnouncer>();
-        builder.Services.AddSingleton<ReplayFileValidator>();
-    }
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
