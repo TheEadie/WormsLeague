26db0a89ff72add5b71a919f8de9f4eb1e132a02
David Eadie <david@eadie.net>
2024-03-22 14:24:52 +0000

    Move service registration to a separate file

---
+++ src/Worms.Hub.Gateway/Services.cs
@@ -0,0 +1,21 @@
+using Worms.Hub.Gateway.API.DTOs;
+using Worms.Hub.Gateway.API.Validators;
+using Worms.Hub.Gateway.Domain;
+using Worms.Hub.Gateway.Domain.Announcers;
+using Worms.Hub.Gateway.Domain.Announcers.Slack;
+using Worms.Hub.Gateway.Storage.Database;
+using Worms.Hub.Gateway.Storage.Files;
+
+namespace Worms.Hub.Gateway;
+
+public static class Services
+{
+    public static IServiceCollection AddGatewayServices(this IServiceCollection builder) =>
+        builder.AddSingleton<IRepository<GameDto>, GamesRepository>()
+            .AddSingleton<IRepository<Replay>, ReplaysRepository>()
+            .AddSingleton<ISlackAnnouncer, SlackAnnouncer>()
+            .AddSingleton<ReplayFileValidator>()
+            .AddSingleton<CliFileValidator>()
+            .AddSingleton<CliFiles>()
+            .AddSingleton<SchemeFiles>();
+}
