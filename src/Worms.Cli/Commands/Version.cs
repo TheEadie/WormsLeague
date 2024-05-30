db03c83163db003ef0777a23a84ef6b59f46cad3
David Eadie <david@eadie.net>
2024-05-30 10:31:47 +0100

    Write out version to stdout rather than logs

--- src/Worms.Cli/Commands/Version.cs
+++ src/Worms.Cli/Commands/Version.cs
@@ -1,6 +1,5 @@
 using System.CommandLine;
 using System.CommandLine.Invocation;
-using Microsoft.Extensions.Logging;
 using Worms.Armageddon.Game;
 using Worms.Cli.CommandLine;
 
@@ -8,10 +7,7 @@ namespace Worms.Cli.Commands;
 
 internal sealed class Version() : Command("version", "Get the current version of the Worms CLI");
 
-internal sealed class VersionHandler(
-    IWormsLocator wormsLocator,
-    CliInfoRetriever cliInfoRetriever,
-    ILogger<VersionHandler> logger) : ICommandHandler
+internal sealed class VersionHandler(IWormsLocator wormsLocator, CliInfoRetriever cliInfoRetriever) : ICommandHandler
 {
     public int Invoke(InvocationContext context) =>
         Task.Run(async () => await InvokeAsync(context).ConfigureAwait(false)).GetAwaiter().GetResult();
@@ -20,15 +16,20 @@ internal sealed class VersionHandler(
     {
         using var span = Telemetry.Source.StartActivity("version");
 
-        var cliInfo = cliInfoRetriever.Get();
-        logger.LogInformation("Worms CLI: {Version}", cliInfo.Version.ToString(3));
+        var (cliVersion, gameVersion) = GetVersions();
 
-        var gameInfo = wormsLocator.Find();
-        var gameVersion = gameInfo.IsInstalled ? gameInfo.Version.ToString(4) : "Not Installed";
-        logger.LogInformation("Worms Armageddon: {Version}", gameVersion);
+        context.Console.WriteLine($"Worms CLI: {cliVersion.ToString(3)}");
+        context.Console.WriteLine($"Worms Armageddon: {gameVersion?.ToString(4) ?? "Not Installed"}");
 
-        _ = span?.SetTag(Telemetry.Attributes.Version_CliVersion, cliInfo.Version.ToString(3));
+        _ = span?.SetTag(Telemetry.Attributes.Version_CliVersion, cliVersion);
         _ = span?.SetTag(Telemetry.Attributes.Version_WormsArmageddonVersion, gameVersion);
         return Task.FromResult(0);
+    }
+
+    private (System.Version CliVersion, System.Version? GameVersion) GetVersions()
+    {
+        var cliInfo = cliInfoRetriever.Get();
+        var gameInfo = wormsLocator.Find();
+        return (cliInfo.Version, gameInfo.IsInstalled ? gameInfo.Version : null);
     }
 }
