23300edcad67ff910bea7df1ddd452bbd6b47bb8
David Eadie <david@eadie.net>
2024-09-20 15:37:41 +0100

    Add fullLog to replay table

--- src/Worms.Hub.Storage/Domain/Replay.cs
+++ src/Worms.Hub.Storage/Domain/Replay.cs
@@ -1,3 +1,3 @@
 namespace Worms.Hub.Storage.Domain;
 
-public sealed record Replay(string Id, string Name, string Status, string Filename);
+public sealed record Replay(string Id, string Name, string Status, string Filename, string? FullLog);
