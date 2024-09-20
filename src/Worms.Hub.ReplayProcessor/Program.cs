0b5393850052e135fc5e65e421f5658d6cf0feef
David Eadie <david@eadie.net>
2024-09-20 14:50:36 +0100

    Add Configuration to the replay processor

--- src/Worms.Hub.ReplayProcessor/Program.cs
+++ src/Worms.Hub.ReplayProcessor/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Worms.Armageddon.Game;
 using Worms.Hub.ReplayProcessor;
 
+var configuration = new ConfigurationBuilder().AddEnvironmentVariables("WORMS_").Build();
 var serviceProvider = new ServiceCollection().AddReplayProcessorServices()
     .AddWormsArmageddonGameServices()
+    .AddLogging(builder => builder.AddConsole())
+    .AddSingleton<IConfiguration>(configuration)
     .BuildServiceProvider();
 
 var processor = serviceProvider.GetService<Processor>();
