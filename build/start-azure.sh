1be76e7f88c58b9e289d82c82ed1aa9fc9a59b9b
David Eadie <david@eadie.net>
2024-11-08 15:21:37 +0000

    Fix Azure start up script

--- build/start-azure.sh
+++ build/start-azure.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set pipefail -euo
 
-pulumi up --cwd ./deployment/worms.davideadie.dev/ --stack test --yes --skip-preview
-JDBC_URL=$(pulumi stack output --cwd ./deployment/worms.davideadie.dev/ --stack test database-jdbc --show-secrets)
-USER=$(pulumi stack output --cwd ./deployment/worms.davideadie.dev/ --stack test database-user --show-secrets)
-PASSWORD=$(pulumi stack output --cwd ./deployment/worms.davideadie.dev/ --stack test database-password --show-secrets)
+pulumi up --cwd ./deployment/Worms.Hub.Infrastructure/ --stack test --yes --skip-preview
+JDBC_URL=$(pulumi stack output --cwd ./deployment/Worms.Hub.Infrastructure/ --stack test database-jdbc --show-secrets)
+USER=$(pulumi stack output --cwd ./deployment/Worms.Hub.Infrastructure/ --stack test database-user --show-secrets)
+PASSWORD=$(pulumi stack output --cwd ./deployment/Worms.Hub.Infrastructure/ --stack test database-password --show-secrets)
 
 flyway info migrate info \
     -url="$JDBC_URL" -user="$USER" -password="$PASSWORD" \
-    -locations="filesystem:./src/database/migrations" \
-    -configFiles=./src/database/flyway.conf \
-    -reportEnabled=false
+    -workingDirectory=./src/database
