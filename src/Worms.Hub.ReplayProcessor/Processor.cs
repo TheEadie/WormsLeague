43dd6c975fd2a0856b06a650394cd2b9f3647bb5
David Eadie <david@eadie.net>
2024-11-22 15:53:45 +0000

    Copy the worms installation from the mount to the filesystem

--- src/Worms.Hub.ReplayProcessor/Processor.cs
+++ src/Worms.Hub.ReplayProcessor/Processor.cs
@@ -39,14 +39,14 @@ internal sealed class Processor(
         // Check game is installed
         if (!GameIsInstalled())
         {
-            const string copyLocation = "/game/";
-            const string installLocation = "/root/.wine/drive_c/";
+            const string copyLocation = "/game";
+            const string installLocation = "/root/.wine/drive_c/WA";
 
             logger.LogInformation($"Game not found. Checking {copyLocation} directory...");
             if (Directory.Exists(copyLocation))
             {
                 logger.LogInformation($"Game files found in {copyLocation} directory. Moving to {installLocation}");
-                Directory.Move(copyLocation, installLocation);
+                CopyDirectory(copyLocation, installLocation, true);
                 logger.LogInformation("Game files moved successfully.");
             }
             else
@@ -106,5 +106,40 @@ internal sealed class Processor(
         }
 
         return true;
+    }
+
+    private static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+    {
+        // Get information about the source directory
+        var dir = new DirectoryInfo(sourceDir);
+
+        // Check if the source directory exists
+        if (!dir.Exists)
+        {
+            throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+        }
+
+        // Cache directories before we start copying
+        var dirs = dir.GetDirectories();
+
+        // Create the destination directory
+        _ = Directory.CreateDirectory(destinationDir);
+
+        // Get the files in the source directory and copy to the destination directory
+        foreach (var file in dir.GetFiles())
+        {
+            var targetFilePath = Path.Combine(destinationDir, file.Name);
+            _ = file.CopyTo(targetFilePath);
+        }
+
+        // If recursive and copying subdirectories, recursively call this method
+        if (recursive)
+        {
+            foreach (var subDir in dirs)
+            {
+                var newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                CopyDirectory(subDir.FullName, newDestinationDir, true);
+            }
+        }
     }
 }
