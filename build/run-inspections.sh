bb2616989226fdec0a32eaa4ecd96dc7400147ff
David Eadie <david@eadie.net>
2025-11-21 14:09:23 +0000

    Create seperate files and merge

--- build/run-inspections.sh
+++ build/run-inspections.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 >&2 echo "Running Rosyln inspections..."
 
-mkdir -p .output
-dotnet build Worms.sln /p:ErrorLog=$(pwd)/.output/roslyn.sarif /p:ErrorLogFormat=SarifV2 /p:EnforceCodeStyleInBuild=true || true
+mkdir -p src/sarif
+dotnet build Worms.sln /p:Sarif=true
