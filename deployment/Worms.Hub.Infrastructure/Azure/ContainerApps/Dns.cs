b7bd4e5cbc4cfe2a28628ab0e6e87abbd67df28e
David Eadie <david@eadie.net>
2026-01-02 13:51:31 +0000

    Fix Obsolete Cloudflare DNS Record Type Usage

--- deployment/Worms.Hub.Infrastructure/Azure/ContainerApps/Dns.cs
+++ deployment/Worms.Hub.Infrastructure/Azure/ContainerApps/Dns.cs
@@ -1,5 +1,6 @@
 using Pulumi;
 using Pulumi.AzureNative.App;
+using Pulumi.Cloudflare.Inputs;
 using Cloudflare = Pulumi.Cloudflare;
 
 namespace Worms.Hub.Infrastructure.Azure.ContainerApps;
@@ -13,26 +14,30 @@ internal static class Dns
         var ipAddress = managedEnvironment.StaticIp;
         var challengeTxtValue =
             managedEnvironment.CustomDomainConfiguration.Apply(x => x?.CustomDomainVerificationId ?? "");
-        var zoneId = Cloudflare.GetZone.Invoke(new() { Name = domain }).Apply(x => x.Id);
+        var zoneId = Cloudflare.GetZone
+            .Invoke(new Cloudflare.GetZoneInvokeArgs { Filter = new GetZoneFilterInputArgs { Name = domain, Match = "all"} })
+            .Apply(x => x.Id);
 
-        _ = new Cloudflare.Record(
+        _ = new Cloudflare.DnsRecord(
             "dns-entry-ip",
-            new()
+            new Cloudflare.DnsRecordArgs
             {
                 ZoneId = zoneId,
                 Name = subdomain,
                 Content = ipAddress,
-                Type = "A"
+                Type = "A",
+                Ttl = 1
             });
 
-        _ = new Cloudflare.Record(
+        _ = new Cloudflare.DnsRecord(
             "dns-entry-txt",
-            new()
+            new Cloudflare.DnsRecordArgs
             {
                 ZoneId = zoneId,
                 Name = "asuid." + subdomain,
                 Content = challengeTxtValue,
-                Type = "TXT"
+                Type = "TXT",
+                Ttl = 1
             });
     }
 }
